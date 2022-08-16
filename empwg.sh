c04a6bc0626a9eaa0c9a1c2b1054d1d9fefd9f46
SanthoshSls <santhoshsls19@gmail.com>
2022-08-16 21:46:02 +0530

    My 3rd commit

--- empwg.sh
+++ empwg.sh
@@ -1,13 +1,17 @@
 #!/bin/bash -x
 
 ft=1;
-emprt=20;
+pt=2;
+emprt=200;
 
 empch=$((RANDOM%3))
 
-if [$empch -eq $ft ]
+if [ $empch -eq $ft ]
 then
 	emphr=8
+elif [ $empch -eq $pt ]
+then
+	emphr=4
 else
 	emphr=0
 fi
