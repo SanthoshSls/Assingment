d0bdce3df2660efaa846573d453f19fd8c4f430b
SanthoshSls <santhoshsls19@gmail.com>
2022-08-16 21:06:57 +0530

    My first commit

--- empwg.sh
+++ empwg.sh
@@ -1,3 +1,13 @@
 #!/bin/bash -x
 
 echo "Welcome to Employee Wage Computation Program on Master Branch"
+
+ip=1
+rd=$((RANDOM%2))
+
+if [ $ip -eq -$eq ]
+then
+   echo "Employee is Present"
+else
+   echo "Employee is Absent"
+fi
